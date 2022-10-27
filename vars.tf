8dd1b3c13acded48fa0c0b0a4c3beaec712960c1
Nagaratna Karetimmannanavar <nagaratnakaretimmannanavar@Nagaratnas-MacBook-Pro.local>
2022-10-26 21:24:37 -0700

    update

--- vars.tf
+++ vars.tf
@@ -1,7 +1,6 @@
 variable "VPC_CIDR" {}
 variable "PUBLIC_SUBNET_CIDR" {}
 variable "PRIVATE_SUBNET_CIDR" {}
-variable "PRIVATE_SUBNET_IDS" {}
 variable "ENV" {}
 variable "AZ" {}
 variable "DEFAULT_VPC_ID" {}
